76114418ddaf84b93cfd83c9e5118bbf4b5dea9a
רון קלקודה <roon@rwns-MacBook-Pro.local>
2022-07-27 13:40:40 +0300

    changed logos location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav>
-            <img className="earth--logo" src="/assets/earth-logo.png" />
+            <img className="earth--logo" src="/earth-logo.png" />
             <p className="main--title">my travel journal</p>
         </nav>
     )
